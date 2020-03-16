50a003994c9c8c43c745a55397a84ae52fab7a84
sitRyo <al15030@shibaura-it.ac.jp>
2020-03-16 20:26:49 +0900

    day2やり直し

--- day3/ipl10.asm
+++ day3/ipl10.asm
@@ -1,103 +1,57 @@
-; helloos
+; haribote-ipl
 ; tab=2
-; boot sector
 
-CYLS EQU 10 ; CYLS=10と定義する。.data sectionに入れなくてもいいのか
-
-  org 0x7c00
-
-; 標準的なFAT12フォーマットフロッピーディスクのための記述
+  org 0x7c00 
+  ; org命令 このプログラムがどこに読み込まれるかを指定する。
+  ; 0x7c00 は補助記憶装置の先頭にある特殊な領域。コンピュータが起動する一番最初に読み込まれる部分。
 
   jmp entry
-  db 0x90
-  db "HARIBOTE"
-  dw 512
-  db 1
-  dw 1
-  db 2
-  dw 224
-  dw 2880
-  db 0xf0
-  dw 9
-  dw 18
-  dw 2
-  dd 0
-  dd 2880
-  db 0,0,0x29
-  dd 0xffffffff
-  db "HARIBOTEOS" ; 11byte
-  db "FAT12   " ; 8byte
-  times 18 db 0 ; 18回0を書き込む(resbでも良くない？良くないか...)
+  db 0x90 
+  db "HARIBOTE" ; ブートセクタの名前
+  dw 512 ; 1セクタの大きさ
+  db 1 ; クラスタの大きさ
+  dw 1 ; FATがどこから始まるか
+  db 2 ; FATの個数
+  dw 224 ; ルートディレクトリ領域の大きさ
+  dw 2880 ; このドライブの大きさ (2880にしなければいけない)
+  db 0xf0 ; メディアのタイプ
+  dw 9 ; FAT領域の長さ
+  dw 18 ; 1トラックにいくつのセクタがあるか
+  dw 2 ; ヘッドの数
+  dd 0 ; パーティションを使っていないので必ず0にする
+  dd 2880 ; このドライブの大きさをもう1度書く
+  db 0,0,0x29 ; よくわからないらしい
+  dd 0xffffffff ; シリアルナンバー?
+  db "HARIBOTEOS " ; ディスクの名前
+  db "FAT12   " ; フォーマットの名前
+  times 18 db 0 ; 18byte開けておく。
 
-; プログラム本体
 entry:
-  mov ax,0
-  mov ss,ax
-  mov sp,0x7c00
-  mov ds,ax
-
-; ディスクを読む
-  mov ax,0x0820 
+  mov ax,0 ; レジスタ初期化
+  mov ss,ax ; ssは直接数値を代入できないので、慣習でaxを介して初期化している。
+  mov sp,0x7c00 ; スタックポインタを0x7c00に
+  mov ds,ax ; 0に初期化ds:なので
   mov es,ax
-  mov ch,0 ; シリンダ0
-  mov dh,0 ; ヘッド0
-  mov cl,2 ; セクタ2
-readloop:
-  mov si,0 ; 失敗回数を数えるレジスタ
-retry:
-  mov ah,0x02 ; ah=0x02ディスク読み込み(bios命令)
-  mov al,1
-  mov bx,0
-  mov dl,0x00 ; Aドライブ
-  int 0x13 ; software intterupt ディスクBIOS呼び出し
-  jnc next ; jnc -> cpuのcarry flagが上がっているか?
-  add si,1
-  cmp si,5
-  jae error ; si >= 5だったらerrorへ飛ぶ
-  mov ah,0x00
-  mov dl,0x00
-  int 0x13 ; driveのリセット
-  jmp retry
-next:
-  mov ax,es ; esの内容をaxに移す
-  add ax,0x0020 ; アドレスを0x200=512加算
-  mov es,ax ; add es, 0x020という命令がないのでこうしている
-  add cl,1
-  cmp cl,18
-  jbe readloop
-  mov cl,1
-  add dh,1
-  cmp dh,2
-  jb readloop
-  mov dh,0
-  add ch,1
-  cmp ch,CYLS
-  jb readloop
 
-; haribote.sysを実行
-  mov [0x0ff0],ch
-  jmp 0xc200
-
-error:
-  mov si,msg
+  mov si,msg ; siにmsgに入れる。
 putloop:
-  mov al,[si]
-  add si,1
-  cmp al,0
+  mov al,[si] ; int 0x0eではalに表示したい文字を入れる
+  add si,1 ; siをインクリメント. msgのポインタが1個ずれる
+  cmp al,0 ; null文字にあったら終わり
   je fin
-  mov ah,0x0e
-  mov bx,15
-  int 0x10
+  mov ah,0x0e ; 1文字表示(int) 
+  mov bx,15 ; color code
+  int 0x10 ; 割り込み
   jmp putloop
+msg:
+  db 0x0a, 0x0a ; 改行を2つ
+  db "hello, world!" ; メッセージ
+  db 0x0a
+  db 0 ; null文字
+  
+  times 0x01fe - ($-$$) db 0 
+
+  db 0x55, 0xaa ; boot sectorであることを保証する
 fin:
   hlt
   jmp fin
-msg:
-  db 0x0a, 0x0a
-  db "hello, world"
-  db 0x0a
-  db 0
-
-  times 0x01fe-($-$$) db 0 ; 0x01feはMBRから見た相対アドレス。絶対アドレスは0x7dfe, $$は0x7c00(MBRの先頭)
-  ; times 510 db 0
-  db 0x55, 0xaa
--- day3/nasmfunc.asm
+++
@@ -1,8 +0,0 @@
-; nasmだけどファイル名はnask
-
-section .text
-  GLOBAL _io_hlt
-
-_io_hlt: ; void io_hlt(void);
-  hlt
-  ret
